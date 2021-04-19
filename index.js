f33a3530204fa896e14d9193d950baaa80b48a97
frvazquez <fredy.vazquez@avattar.com>
2021-04-19 18:26:48 -0500

    ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var mongoose = require('mongoose');
 
+var app =  require('./app');
 var port = process.env.port || 3789;
 
 mongoose.Promise = global.Promise;
@@ -9,6 +10,11 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/productos', { useNewUrlParser: true })
     .then(() => {
         console.log('La conexion de base de datos a mongodb se ha realizado');
+
+        app.listen(port, () => {
+            console.log('Los servicios de NodeJs esta corriendo...');
+        });
+
     })
     .catch( err => {
         console.error('Error de conexion.');
